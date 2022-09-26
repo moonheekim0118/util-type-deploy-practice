20bf70c651a0453a53b775c9e6d61196c20db62c
moonheekim0118 <hellomooneekim@gmail.com>
2022-09-27 01:20:06 +0900

    셋팅 추가

--- lib/index.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export type PropsWithReactNodeChildren<P = unknown> = P & {
-  children: ReactNode;
-};
-
-export type PropsWithFunctionChildren<P = unknown> = P & {
-  children: (args: any) => any;
-};
